f5b63e36a911592f2c1b9785ab9a91b7428e0626
Fernando Varani <fervarani@gmail.com>
2014-10-06 15:03:41 -0300

    working on sender

---
+++ app/models/stat_hat_sender.rb
@@ -0,0 +1,17 @@
+class StatHatSender
+
+  def initialize(stat_name, user_token, stat_value, event_type)
+    @stat_name = stat_name
+    @user_token = user_token
+    @stat_value = event_type
+    @event_type = event_type
+  end
+
+  def send!
+    case @event_type
+    when 'value' then StatHat::API.ez_post_value(@stat_name, @user_token, @stat_value)
+    when 'count' then StatHat::API.ez_post_count(@stat_name, @user_token, @stat_value)
+    end
+  end
+
+end
---
+++ test/models/stat_hat_sender_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StatHatSenderTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
