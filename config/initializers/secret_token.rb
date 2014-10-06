6e3953cda944ae68ee10fcb7c6e36e7f6a76156f
F. Varani <fervarani@gmail.com>
2014-10-06 14:01:23 +0000

    creating app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShIntegration::Application.config.secret_key_base = '7e39a5b92fa006dc866d58bf684e8cc163452423047945273609dd9370d86581fb78c31ad9e31bdc6cbd6f5ae39861940b7b4d75066e625a26d9fdd5f3acd472'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShIntegration::Application.config.session_store :cookie_store, key: '_sh-integration_session'
